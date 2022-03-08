808d6995373884cf704b5266505d6a4ab01badc8
Evan Relf <evan@evanrelf.com>
2022-03-08 13:42:35 -0800

    Draw grid of rectangles

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,30 @@ fn main() {
 
 fn setup(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn_bundle(SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(0.25, 0.25, 0.75),
-            custom_size: Some(Vec2::new(50.0, 50.0)),
-            ..Default::default()
-        },
-        ..Default::default()
-    });
+
+    let rows = 6;
+    let columns = 5;
+    let size = Vec3::new(50.0, 50.0, 0.0);
+    let spacing = 20.0;
+    let width = columns as f32 * (size.x + spacing) - spacing;
+    let offset = Vec3::new(-(width - size.x) / 2.0, 100.0, 0.0);
+
+    for row in 0..rows {
+        let y_position = row as f32 * (size.y + spacing);
+        for column in 0..columns {
+            let position = Vec3::new(column as f32 * (size.x + spacing), y_position, 0.0) + offset;
+            commands.spawn_bundle(SpriteBundle {
+                sprite: Sprite {
+                    color: Color::BLACK,
+                    ..Default::default()
+                },
+                transform: Transform {
+                    translation: position,
+                    scale: size,
+                    ..Default::default()
+                },
+                ..Default::default()
+            });
+        }
+    }
 }
